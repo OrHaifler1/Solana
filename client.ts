185e8b19a5d700214c6b677fa99be3879df07d7f
OrHaifler1 <127110549+OrHaifler1@users.noreply.github.com>
2023-07-05 13:20:07 +0300

    Create client.ts

---
+++ client.ts
@@ -0,0 +1,19 @@
+// create an empty transaction
+const transaction = new web3.Transaction();
+
+// add a hello world program instruction to the transaction
+transaction.add(
+  new web3.TransactionInstruction({
+    keys: [],
+    programId: new web3.PublicKey(pg.PROGRAM_ID),
+  })
+);
+
+// send the transaction to the Solana cluster
+console.log("Sending transaction...");
+const txHash = await web3.sendAndConfirmTransaction(
+  pg.connection,
+  transaction,
+  [pg.wallet.keypair]
+);
+console.log("Transaction sent with hash:", txHash);
